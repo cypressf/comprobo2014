81d9c5a10b506bee21dd016d5a0e2b7a0d1370e1
Cypress Frankenfeld <iamcypress@gmail.com>
2014-09-16 15:06:56 -0400

    use zone of danger instead of closest point

--- src/warmup/scripts/warmup.py
+++ src/warmup/scripts/warmup.py
@@ -10,55 +10,89 @@ import math
 TAU = 2 * math.pi
 MAX_LINEAR_SPEED = 0.2
 MAX_ANGULAR_SPEED = 0.2
+DANGER_ZONE_LENGTH = 1.0
+DANGER_ZONE_WIDTH = 0.5
 
 
 def degrees(radians):
     radians %= TAU
     degree_index = math.degrees(radians)
-    if degree_index < 0:
-        degree_index = 360 - degree_index
     return int(degree_index)
 
 
 def ranges_to_points(ranges):
     points = []
-    for angle, radius in enumerate(ranges[269::] + ranges[0::269]):
-        points.append(Point(radius=radius, angle=angle))
+    for angle, radius in enumerate(ranges):
+        points.append(Point(length=radius, angle=angle))
     return points
+
+
+def is_in_front_left(point):
+    return 0 < point.angle_radians <= TAU * 1/4.0
+
+
+def is_in_back_left(point):
+    return TAU * 1/4.0 < point.angle_radians <= TAU * 1/2.0
+
+
+def is_in_back_right(point):
+    return TAU * 1/2.0 < point.angle_radians <= TAU * 3/4.0
+
+
+def is_in_front_right(point):
+    return TAU * 3/4.0 < point.angle_radians <= TAU
+
+
+def is_in_danger_zone(point):
+    a = DANGER_ZONE_LENGTH * math.sin(point.angle_radians)
+    b = DANGER_ZONE_WIDTH * math.cos(point.angle_radians)
+    max_radius = (DANGER_ZONE_LENGTH * DANGER_ZONE_WIDTH) / math.sqrt(a**2 + b**2)
+    return point.length < max_radius
 
 
 class Point:
 
-    def __init__(self, radius=0.0, angle=0.0):
-        self.radius = radius # meters
+    def __init__(self, length=0.0, angle=0.0):
+        self.length = length # meters
         self.angle_degrees = angle
         self.angle_radians = math.radians(angle)
 
     def __lt__(self, other):
-        return self.radius < other.radius
+        return self.length < other.radius
 
     def __str__(self):
-        return "radius: %.2f  angle: %.3f" % (self.radius, self.angle_radians / TAU)
+        return "radius: %.2f  angle: %.3f" % (self.length, self.angle_radians / TAU)
+
 
 class Controller:
 
     def __init__(self):
-        self.points = []
+        self.front_points = []
         self.proportion_constant = 1.0
         self.goal = 1.0  # meters
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.sub = None
-        self.closest_point = Point(angle=0.0, radius=100.0)
-        self.move = self.move_forward
+        self.move = self.turn_right
         self.running = False
         signal.signal(signal.SIGINT, self.signal_handler)
         rospy.init_node('controller', anonymous=True)
 
-    def find_path(self):
-        # if self.mean_distance > 1.0:
-        #     self.move = self.move_forward
+    def is_in_danger(self):
+        return len(self.get_danger_points()) > 0
 
-        if self.closest_point.angle_radians <= TAU * 1/4.0:
+    def get_danger_points(self):
+        return [point for point in self.front_points if is_in_danger_zone(point)]
+
+    def find_path(self):
+        danger_points = self.get_danger_points()
+        left_danger_points = [point for point in danger_points if is_in_front_left(point)]
+        right_danger_points = [point for point in danger_points if is_in_front_right(point)]
+
+        if not danger_points:
+            self.move = self.move_forward
+            return self.move_forward()
+
+        if len(left_danger_points) < len(right_danger_points):
             self.move = self.turn_left
             return self.turn_left()
         else:
@@ -66,24 +100,22 @@ class Controller:
             return self.turn_right()
 
     def turn_left(self):
-        if self.closest_point.radius > 1.0:
+        if not self.is_in_danger():
             self.move = self.move_forward
             return self.move_forward()
 
         return Twist(angular=Vector3(z=MAX_ANGULAR_SPEED))
 
     def turn_right(self):
-        if self.closest_point.radius > 1.0:
+        if not self.is_in_danger():
             self.move = self.move_forward
             return self.move_forward()
+        rospy.loginfo(len(self.get_danger_points()))
 
         return Twist(angular=Vector3(z=-MAX_ANGULAR_SPEED))
 
     def move_forward(self):
-        # if self.mean_distance < 1.0:
-        #     self.move = self.find_path
-
-        if self.closest_point.radius <= 1.0:
+        if self.is_in_danger():
             self.move = self.find_path
 
         return Twist(linear=Vector3(x=MAX_LINEAR_SPEED))
@@ -93,13 +125,9 @@ class Controller:
         Process new LaserScan message.
         """
         points = ranges_to_points(laser_scan_msg.ranges)
-        front_points = [point for point in points if TAU * 1/8.0 < point.angle_radians < TAU * 3/8.0]
-        valid_front_points = [point for point in front_points if 0 < point.radius < 8]
 
-        if valid_front_points:
-            self.closest_point = min(valid_front_points)
-
-        rospy.loginfo(self.closest_point)
+        front_points = [point for point in points if is_in_front_left(point) or is_in_front_right(point)]
+        self.front_points = [point for point in front_points if 0 < point.length < 5]
         self.pub.publish(self.move())
 
     def run(self):
